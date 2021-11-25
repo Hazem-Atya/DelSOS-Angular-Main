2b15cc8e3367d8bda3cb6ac0cbf4d6e70adb8060
rihemebh <rihemebenhassan@insat.u-carthage.tn>
2021-11-25 23:59:35 +0100

    steps

---
+++ .history/src/app/register/register.component_20211125231138.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+    data : Date = new Date();
+    focus: any;
+    focus1: any;
+steps : Array
+ constructor() { }
+
+    ngOnInit() {
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.add('login-page');
+
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.add('navbar-transparent');
+    }
+    ngOnDestroy(){
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.remove('login-page');
+
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.remove('navbar-transparent');
+    } 
+
+
+}
